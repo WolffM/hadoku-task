fdb141e206f013dbad365864cf5496f749b17bcb
Matthaeus <mawolf@microsoft.com>
2025-10-27 14:50:26 -0700

    Use @wolffm/themes package instead of local theme CSS

--- src/app/entry.tsx
+++ src/app/entry.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import '@wolffm/themes/themes.css'
 import '../styles/index.css'
 
 // Service Worker disabled - parent app handles GitHub integration
